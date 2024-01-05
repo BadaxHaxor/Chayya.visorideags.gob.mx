ae4ffd41539f266654a1df069d740447c9b9f80f
backlinks <98331189+BadaxHaxor@users.noreply.github.com>
2024-01-06 04:54:11 +0700

    Update asu.php
    Signed-off-by: backlinks <98331189+BadaxHaxor@users.noreply.github.com>

--- asu.php
+++ asu.php
@@ -1,7 +1,6 @@
 <div style="display: none;">
-<a href="https://slot88.ubinus.ac.id/">slot88</a>
-<a href="https://slot88.ubinus.ac.id/">slot-gacor</a>
-<a href="https://slot88.ubinus.ac.id/">slot77</a>
-<a href="https://slot88.ubinus.ac.id/">slot</a>
-<a href="https://slot88.ubinus.ac.id/">slotters</a>
+<a href="https://slot88.ubinus.ac.id/">
+<a href="https://www.liceoerika.edu.co/">
+<a href="https://slot88.unhamka.ac.id/">
+<a href="https://slot88.unindira.ac.id">
 </div>
